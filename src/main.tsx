6e146dedf02a8d6e9cb9149aa457c94bac615e9a
hasini-reddyy <hasinireddy1011@gmail.com>
2026-01-04 15:28:56 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,15 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App.jsx";
+import GoogleSignIn from "./components/GoogleSignIn.jsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App />
+    <GoogleSignIn />
+  </React.StrictMode>
+);
